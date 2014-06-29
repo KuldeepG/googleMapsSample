0e943a208e491d256322a7d423c56955b34b50b1
Kuldeep Ghogre <kuldeep.ghogre@gmail.com>
2014-06-29 16:15:04 +0530

    Moving the google maps javascript to the controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,4 +5,14 @@ angular.module('SportBookingAssignment.controllers', [])
     $scope.isActive = function (currentLocation) { 
       return currentLocation === $location.path();
     };
+  }])
+  .controller('Map', ['$scope', function(){
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(52.4837, 13.3503),
+        new google.maps.LatLng(52.5797, 13.4163));
+
+    map.fitBounds(defaultBounds);
   }]);

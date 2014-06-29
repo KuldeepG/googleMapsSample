a0b015ff33f2bd71513b2108d96dab9bc5d8fa7e
Kuldeep Ghogre <kuldeep.ghogre@gmail.com>
2014-06-29 15:50:22 +0530

    Adding the missing controller for navbar

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,8 @@
 'use strict';
 
-angular.module('SportBookingAssignment.controllers', []);
+angular.module('SportBookingAssignment.controllers', [])
+  .controller('Navigation', ['$scope', '$location', function($scope, $location) {
+    $scope.isActive = function (currentLocation) { 
+      return currentLocation === $location.path();
+    };
+  }]);
